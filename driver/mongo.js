4e6607a311b57c5fb73e66e6a64a26e08328f49a
Shahzaib Sheikh <shahzaib4213@gmail.com>
2020-03-10 18:27:56 +0500

    Fixes indentation issues

--- driver/mongo.js
+++ driver/mongo.js
@@ -1,14 +1,26 @@
 const { createDirectory } = require("../helper/directory");
 const { exec } = require("child_process");
 
-const mongoDump = async function({ host, port, username, password, database, temp_dir, temp_file_path }) {
+const mongoDump = async function({
+  host,
+  port,
+  username,
+  password,
+  database,
+  temp_dir,
+  temp_file_path
+}) {
   await createDirectory(temp_dir);
   const cmd = `mongodump --host=${host} --port=${port} --username=${username} --password=${password} -d ${database} --gzip --archive=${temp_file_path}`;
-  console.log(`Starting taking dump of [type]mongo [host]${host}:${port} [database]${database} to [temp-file]${temp_file_path}`);
+  console.log(
+    `Starting taking dump of [type]mongo [host]${host}:${port} [database]${database} to [temp-file]${temp_file_path}`
+  );
   return new Promise(function(res, rej) {
     exec(cmd, (err, stdout, stderr) => {
       if (err) return rej(err);
-      console.log(`Done with taking dump of [type]mongo [host]${host}:${port} [database]${database} to [temp-file]${temp_file_path}`);
+      console.log(
+        `Done with taking dump of [type]mongo [host]${host}:${port} [database]${database} to [temp-file]${temp_file_path}`
+      );
       res(temp_file_path);
     });
   });
